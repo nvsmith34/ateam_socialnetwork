6b775cd4a6a074b4536007a2d698ca612ff878f5
humanshapedopticalillusion <56094193+humanshapedopticalillusion@users.noreply.github.com>
2019-12-08 20:42:13 -0600

    Update SocialNetwork.java

--- SocialNetwork.java
+++ SocialNetwork.java
@@ -1,128 +1,162 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Arrays;
 
-public class SocialNetwork implements SocialNetworkADT {
-	private Graph graph;
-	//private Person mainUser;
-	private File log;
-	
-	public SocialNetwork() {
-		graph = new Graph();
-		log = new File("log.txt");
-	}
-	@Override
-	public boolean addFriends(String user, String friend) {
-		
-		return false;
-	}
+public class SocialNetwork {
+  Graph network;
 
-	@Override
-	public boolean removeFriends(String user, String friend) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+  public SocialNetwork() {
+    network = new Graph();
+  }
 
+  public boolean addFriends(String person1, String person2) {
+    if (person1 == null || person2 == null || person1.length() == 0 || person2.length() == 0) {
+      return false;
+    }
+    Person per1 = network.getNode(person1);
+    Person per2 = network.getNode(person2);
+    if (per1 == null) {
+      per1 = new Person(person1);
+    }
+    if (per2 == null) {
+      per2 = new Person(person2);
+    }
+    if (network.addEdge(per1, per2) == 1) {
+      return true;
+    }
+    return false;
+  }
+  
+  public boolean removeFriends(String person1, String person2) {
+    if (person1 == null || person2 == null || person1.length() == 0 || person2.length() == 0) {
+      return false;
+    }
+    Person per1 = network.getNode(person1);
+    Person per2 = network.getNode(person2);
+    if (per1 == null) {
+      return false;
+    }
+    if (per2 == null) {
+      return false;
+    }
+    if (network.removeEdge(per1, per2) == 1) {
+      return true;
+    }
+    return false;
+  }
+  
+  public boolean addUser(String user) {
+    if (user == null || user.length() == 0) {
+      return false;
+    }
+    Person person = new Person(user);
+    if (network.addNode(person) == 1) {
+      return true;
+    }
+    return false;
+  }
+  
+  public boolean removeUser(String user) {
+    if (user == null || user.length() == 0) {
+      return false;
+    }
+    Person person = new Person(user);
+    if (network.removeNode(person) == 1) {
+      return true;
+    }
+    return false;
+  }
+  
+  public Set<String> getFriends(String user) {
+    if (user == null || user.length() == 0) {
+      return null;
+    }
+    Person person = network.getNode(user);
+    if (person == null) {
+      return null;
+    }
+    List<Person> friendList = network.getNeighbors(person);
+    Set<String> friends = new HashSet<String>();
+    for (Person p : friendList) {
+      friends.add(p.getName());
+    }
+    return friends;
+  }
+  
+  public Set<Graph> getConnectedComponents() {
+    Set<Person> allPeople = network.getAllNodes();
+    Set<Graph> connectedGraphs = new HashSet<Graph>();
+    while (allPeople.size() != 0) {
+      Graph currGraph =  new Graph();
+      Queue<Person> q = new LinkedList<Person>();
+      q.add((Person)allPeople.toArray()[0]);
+      while (!q.isEmpty()) {
+        Person currNode = q.poll();
+        currGraph.addNode(currNode);
+        allPeople.remove(currNode);
+        for (Person p : network.getNeighbors(currNode)) {
+          if (currGraph.getNode(p.getName()) == null) {
+            currGraph.addEdge(currNode, p);
+            q.add(p);
+            allPeople.remove(p);
+          }
+        }
+      }
+      connectedGraphs.add(currGraph);
+    }
+    return connectedGraphs;
+  }
+  
+  private class inPq implements Comparable<inPq> {
+    private int dist;
+    private Person thePerson;
+    private boolean visited;
 
-	@Override
-	public boolean addUser(String user) {
-		Person person = new Person(user);
-		
-		return graph.addNode(person) >0;
-	
-		
-	}
-
-	@Override
-	public boolean removeUser(String user) {
-		return graph.removeNode(graph.getNode(user))>0;
-		
-	}
-
-	@Override
-	public Set<Person> getFriends(String user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public Set<Person> getMutualFriends(String user1, String user2) {
-		Set<Person> mutual = getFriends(user1);
-				mutual.retainAll(getFriends(user2));
-		return mutual;
-	}
-
-	@Override
-	public List<Person> getShortestPath(String user1, String user2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Graph> getConnectedComponents() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Person loadFromFile(File filename) throws FileNotFoundException {
-		Scanner scnr = new Scanner(filename);
-		Person mainUser = null;
-		while(scnr.hasNextLine()) {
-			String input = scnr.nextLine();
-			String[] command = input.split(" ");
-			switch(command[0]) {
-			case "a" : if(command.length == 3)
-							addFriends(command[1],command[2]);
-						else if(command.length == 2)
-							addUser(command[1]);
-						
-						break;
-			case "r": if(command.length==3)
-							removeFriends(command[1],command[2]);
-					  else if(command.length ==2)
-						  removeUser(command[1]);
-			
-			case "s": if(command.length==2)
-						mainUser = graph.getNode(command[1]);
-						
-			}
-		}
-		return mainUser;
-	}
-	
-	/**
-	public void setMainUser(String user) {
-		mainUser = graph.getNode(user);
-	}
-	
-	public Person getMainUser(String user) {
-		return mainUser;
-	}
-	 * @throws IOException 
-	*/
-
-	@Override
-	public void saveToFile(File filename) throws IOException {
-		
-		FileInputStream log = new FileInputStream(this.log);
-		FileOutputStream out = new FileOutputStream(filename);
-		
-		byte[]buffer = new byte[1024];
-		int length;
-		
-		while((length = log.read(buffer))>0) {
-			out.write(buffer,0,length);
-		}
-		
-		out.close();
-		log.close();
-	}
-
-
+    public inPq(Person p, int dist) {
+      thePerson = p;
+      this.dist = dist;
+    }
+    public void visit() {
+      visited = true;
+    }
+    public Person getPerson() {
+      return thePerson;
+    }
+    public boolean getVisited() {
+      return visited;
+    }
+    @Override
+    public int compareTo(inPq comp) {
+      if (dist == -1) {
+        return 1;
+      }
+      if (comp.dist == -1) {
+        return -1;
+      }
+      return dist - comp.dist;
+    }
+    
+  }
+  
+  public List<Person> getShortestPath(String person1, String person2) {
+    if (person1 == null || person2 == null || person1.length() == 0 || person2.length() == 0) {
+      return null;
+    }
+    Person per1 = network.getNode(person1);
+    Person per2 = network.getNode(person2);
+    if (per1 == null || per2 == null) {
+      return null;
+    }
+    List<inPq> bitch = new LinkedList<inPq>();
+    for (Person p : network.getAllNodes()) {
+      inPq qAddition = new inPq(p, -1);
+      bitch.add(qAddition);
+    }
+    PriorityQueue<inPq> pq = new PriorityQueue<inPq>();
+    return null;
+  }
 }
